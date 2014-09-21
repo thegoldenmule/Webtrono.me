fd82f4a46ff1eec16fa770d377790d6f0b2cb409
Benjamin Jordan <bmj465@gmail.com>
2014-09-21 16:19:30 -0500

    Curve controls

--- js/thegoldenmule/TimerCurveView.js
+++ js/thegoldenmule/TimerCurveView.js
@@ -15,10 +15,10 @@
         // draws background
         var width = container.offsetWidth;
         var height = container.offsetHeight;
-        var rect = paper.rect(0, 0, width, height);
-        rect.attr('fill', '#CCCCCC');
-        rect.attr('stroke', '#AAAAAA');
-        rect.attr('stroke-width', 3);
+        this._bg = paper.rect(0, 0, width, height);
+        this._bg.attr('fill', '#CCCCCC');
+        this._bg.attr('stroke', '#AAAAAA');
+        this._bg.attr('stroke-width', 3);
 
         // draw playhead
         var playhead = paper.path('m0,0l0,' + height);
@@ -28,10 +28,11 @@
         // draw curve
         this._curve = paper.path(
             'M0,0' + height
-            + 'Q' + width / 2 + ',0' + ',' + width + ',' + height);
+            + 'Q' + width / 2 + ',' + height + ',' + width + ',' + height);
+        this._curve.attr('stroke', '#000000');
 
         // buttons
-        $('timer-curve-button').click(function(event) {
+        $('.timer-curve-button').click(function(event) {
             var attributes = event.target.attributes;
             for (var i = 0, len = attributes.length; i < len; i++) {
                 var name = attributes[i].name;
@@ -39,9 +40,15 @@
                 if (name === 'timer-value') {
                     var percentile = parseInt(value);
                     if (0 === percentile) {
-
+                        scope._curve.remove();
+                        scope._curve = paper.path(
+                            'M0,0' + height
+                                + 'Q' + width / 2 + ',' + height + ',' + width + ',' + height);
                     } else {
-                        
+                        scope._curve.remove();
+                        scope._curve = paper.path(
+                            'M0,0' + height
+                            + 'Q' + width / 2 + ',-' + (percentile / 50 * 40) + ',' + width + ',' + height);
                     }
                     break;
                 }
